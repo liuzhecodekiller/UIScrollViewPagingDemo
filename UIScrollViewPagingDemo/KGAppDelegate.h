4f347a2e0603cd2dd2fd91730d16bdde4d15192e
KG <qianfeng@KG.local>
2015-06-18 10:07:30 +0800

    Initial Commit

---
+++ UIScrollViewPagingDemo/KGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KGAppDelegate.h
+//  UIScrollViewPagingDemo
+//
+//  Created by KG on 15/6/18.
+//  Copyright (c) 2015年 KG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIScrollViewPagingDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UIScrollViewPagingDemo
+//
+//  Created by KG on 15/6/18.
+//  Copyright (c) 2015年 KG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KGAppDelegate class]));
+    }
+}
